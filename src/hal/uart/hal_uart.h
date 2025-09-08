192233a3708e8301abac24ace9464f69f73cf162
Gofnax <glater.emil@gmail.com>
2025-09-08 14:08:32 +0100

    Start working on hal_uart.h

--- src/hal/uart/hal_uart.h
+++ src/hal/uart/hal_uart.h
@@ -1,6 +1,16 @@
 #ifndef HAL_UART_H
 #define HAL_UART_H
 
+typedef struct
+{
+    char*       path;           // Path to the UART device, e.g. "/dev/ttyAMA0"
+    int         fd;             // File descriptor for the UART device
+    speed_t     baud;           // Baud rate or BPS for the device communication
+    tcflag_t    word_size;      // 
+    uint8_t     stop_bits;
+    uint8_t     parity;
+} UARTDevice;
+
 int main(void);
 
 #endif
