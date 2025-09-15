2373375e6b5be72e58ef5d86a28b7b75bf6fc80c
Gofnax <glater.emil@gmail.com>
2025-09-15 15:40:45 +0100

    Make progress with 'hal_uart_init()'

--- src/hal/uart/hal_uart.c
+++ src/hal/uart/hal_uart.c
@@ -68,4 +68,46 @@ int hal_uart_init()
     uart_devices[eUART2_DEVICE].word_size = word_size_options[eUART2_BITS_PER_BYTE_CONFIG];
     uart_devices[eUART2_DEVICE].stop_bits = stop_bits_options[eUART2_STOP_BIT_CONFIG];
     uart_devices[eUART2_DEVICE].parity = parity_options[eUART2_PARITY_BIT_CONFIG];
+
+    // Here we set the baud, flags (we don't need any), word size, stop bits, and parity bits for each UART device
+    // All the bits configuration is done via the termios struct with its flags and the 'tcsetattr' function
+    // The baud is set using 'cfsetospeed' and 'cfsetispeed' functions
+    // We get the file descriptor for each device after we configure it and use the POSIX 'open' function
+
+    struct termios temp_config = { 0 };
+
+    temp_config.c_cflag |= CLOCAL | CREAD; // Prevents waiting for modem connection and enables reading input from the terminal
+
+    temp_config.c_cflag |= uart_devices[eUART0_DEVICE].word_size;   // Sets the bits per byte
+
+    if(uart_devices[eUART0_DEVICE].stop_bits == 0)
+    {
+       temp_config.c_cflag &= (tcflag_t)~(CSTOPB);  // Ensures CSTOPB bit is 0  (sets use of one stop bit)
+    }
+    else
+    {
+        temp_config.c_cflag |= CSTOPB;              // Sets CSTOPB bit to 1 (sets use of two stop bits)
+    }
+
+    if(uart_devices[eUART0_DEVICE].parity == 0)
+    {
+        temp_config.c_cflag &= (tcflag_t)~(PARENB);     // Ensures PAREND bit is 0 (disables parity generation and detection)
+        temp_config.c_iflag &= (tcflag_t)~(INPCK);      // Ensures INPCK bit is 0 (disables input parity checking)
+    }
+    else
+    {
+        temp_config.c_cflag |= PARENB;                  // Sets PAREND bit to 1 (enables parity generation and detection)
+        if(uart_devices[eUART0_DEVICE].parity == 1)
+        {
+            temp_config.c_cflag &= (tcflag_t)~(PARODD); // Ensures PARODD bit is 0 (sets parity to even)
+        }
+        else
+        {
+            temp_config.c_cflag |= PARODD;              // Sets PARODD bit to 1 (sets parity to odd)
+        }
+        temp_config.c_iflag |= INPCK;                   // Sets INPCK bit to 1 (enables input parity checking)
+        temp_config.c_iflag |= IGNPAR;                  // Sets IGNPAR bit to 1 (sets framing or parity errors to be ignored)
+    }
+
+    
 }
