49de3a2a8c6bb510764a0261d7f208856b497292
Gofnax <glater.emil@gmail.com>
2025-12-05 09:36:28 +0000

    Test hal_gpio_read() function (it works)

--- experiments/led_flash_new.c
+++ experiments/led_flash_new.c
@@ -12,6 +12,12 @@ void handle_sigint(int sig) // function that handles SIGINT interrupt (happens w
     stop = 1;
 }
 
+/*  To test the GPIO library, we enable devices 0 and 1 configured as INPUT on line 17
+    and OUTPUT on line 27, respectively. Then we use a breadboard, connecting line 17
+    to the + column, the GND to the - column, attach a resistor and a LED down the column,
+    and in its end connect the + to line 27 on the Pi. This way we have message printing
+    to the terminal and a LED flashing for physical feedback.
+*/
 int main(void)
 {
     signal(SIGINT, handle_sigint);
@@ -32,13 +38,33 @@ int main(void)
         printf("Failed to write: %d\n", ret_val);
         return -1;
     }
-
-    while(!stop)
+    
+    int read_val = 0;
+    hal_gpio_read(1, &read_val);
+    if(read_val == 0)
     {
-        hal_gpio_write(0, 1);
-        nanosleep(&ts, NULL);
+        printf("Read low signal\n");
+    }
+
+    hal_gpio_write(0, 1);   // Write '1' to line 17
+    nanosleep(&ts, NULL);
+    hal_gpio_read(1, &read_val);
+
+    if(read_val == 1)
+    {
+        printf("Read works\n");
         hal_gpio_write(0, 0);
-        nanosleep(&ts, NULL);
+        while(!stop)
+        {
+            hal_gpio_write(0, 1);
+            nanosleep(&ts, NULL);
+            hal_gpio_write(0, 0);
+            nanosleep(&ts, NULL);
+        }
+    }
+    else
+    {
+        printf("Read failed\n");
     }
 
     return 0;
